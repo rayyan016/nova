8c9230b880a1be2c1142746a535176e09ee1ca3b
Rayyan Aslam <102043379+rayyan016@users.noreply.github.com>
2022-08-12 18:15:25 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,65 @@
+
+function scoreDisplayOnLoad()
+{
+    if(typeof(sessionStorage.clickcount) == "undefined")
+    {
+        document.getElementById("scC").innerHTML = "Correct = 0";    
+    }
+    else
+        document.getElementById("scC").innerHTML = "Correct = " + sessionStorage.clickcount;
+}
+
+function scoreDisplayCorrect()
+{
+    if(typeof(Storage) !== "undefined") 
+    {
+        if (sessionStorage.clickcount) 
+        {
+          sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+        } 
+        else 
+        {
+          sessionStorage.clickcount = 1;
+        }
+        document.getElementById("scC").innerHTML = "Correct = " + sessionStorage.clickcount;
+    } 
+    else 
+    {
+        document.getElementById("scC").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+
+}
+
+// function scoreDisplayIncorrect()
+// {
+//     if(typeof(Storage) !== "undefined") 
+//     {
+//         if (sessionStorage.clickcount) 
+//         {
+//           sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+//         } 
+//         else 
+//         {
+//           sessionStorage.clickcount = 1;
+//         }
+//         document.getElementById("scI").innerHTML = "Incorrect = " + sessionStorage.clickcount;
+//     } 
+//     else 
+//     {
+//         document.getElementById("scI").innerHTML = "Sorry, your browser does not support web storage...";
+//     }
+
+// }
+
 function right()
 {
     var i = document.getElementById("correct");
     var j = document.getElementById("incorrect");
     i.style.display="block";
     j.style.display="none";
+    // score+=1;
+    // var scorevar = document.getElementById("scC");
+    // scorevar.innerHTML = " Correct = "+score;
 }
 
 function wrong()
@@ -12,6 +68,7 @@ function wrong()
     var j = document.getElementById("correct");
     i.style.display="block";
     j.style.display="none";
+    // scoren-=1;
 }
 
 function randomise()
